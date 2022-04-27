5d59d5b5d6a63650d3257ec86871177925385f76
davidcoats <david@getsafetycone.com>
2022-04-27 15:31:54 -0700

    Interim changes.

---
+++ source/R5T.Ives.Base/Code/Services/Definitions/IConfigurationNameProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Ives
+{
+    [ServiceDefinitionMarker]
+    public interface IConfigurationNameProvider : IServiceDefinition
+    {
+        string GetConfigurationName();
+    }
+}
